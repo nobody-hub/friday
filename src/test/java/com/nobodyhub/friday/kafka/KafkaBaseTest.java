b1fd463b036893eeeffe68643d529f5846a4bf32
javis-yan <yan_h@hotmail.com>
2018-01-23 22:17:46 +0800

    Test Spring+Kafka

---
+++ src/test/java/com/nobodyhub/friday/kafka/KafkaBaseTest.java
@@ -0,0 +1,41 @@
+package com.nobodyhub.friday.kafka;
+
+import com.google.common.collect.Maps;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Map;
+
+/**
+ * @author Ryan
+ * @since 23/01/2018
+ */
+public abstract class KafkaBaseTest {
+    protected Map<String, Object> producerProps() {
+        Map<String, Object> props = Maps.newHashMap();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
+    protected Map<String, Object> consumerProps() {
+        Map<String, Object> props = Maps.newHashMap();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "100");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return props;
+    }
+}
---
+++ src/test/java/com/nobodyhub/friday/kafka/KafkaPojoTest.java
@@ -0,0 +1,76 @@
+package com.nobodyhub.friday.kafka;
+
+import com.google.common.collect.Maps;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.Test;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.listener.MessageListener;
+import org.springframework.kafka.listener.config.ContainerProperties;
+
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Ryan
+ * @since 22/01/2018
+ */
+public class KafkaPojoTest extends KafkaBaseTest {
+
+    private Logger logger = Logger.getLogger(KafkaPojoTest.class.getName());
+
+    @Test
+    public void testAutoCommit() throws Exception {
+        logger.info("Start auto");
+        ContainerProperties containerProperties = new ContainerProperties("topic1", "topic2");
+        final CountDownLatch latch = new CountDownLatch(4);
+        containerProperties.setMessageListener(new MessageListener<Integer, String>() {
+            public void onMessage(ConsumerRecord<Integer, String> data) {
+                logger.info("received:" + data);
+                latch.countDown();
+            }
+        });
+        KafkaMessageListenerContainer<Integer, String> container = createContainer(containerProperties);
+        container.setBeanName("testAuto");
+        container.start();
+        Thread.sleep(1000);
+        KafkaTemplate<Integer, String> template = createTemplate();
+        template.setDefaultTopic("topic1");
+        template.sendDefault(0, "foo");
+        template.sendDefault(2, "bar");
+        template.sendDefault(0, "baz");
+        template.sendDefault(2, "qux");
+        template.flush();
+        assertTrue(latch.await(60, TimeUnit.SECONDS));
+        container.stop();
+        logger.info("Stop auto");
+    }
+
+    private KafkaTemplate<Integer, String> createTemplate() {
+        Map<String, Object> props = producerProps();
+        DefaultKafkaProducerFactory<Integer, String> pf = new DefaultKafkaProducerFactory<>(props);
+        KafkaTemplate<Integer, String> template = new KafkaTemplate<>(pf);
+        return template;
+    }
+
+    private KafkaMessageListenerContainer<Integer, String> createContainer(ContainerProperties containerProperties) {
+        Map<String, Object> props = consumerProps();
+        DefaultKafkaConsumerFactory<Integer, String> cf = new DefaultKafkaConsumerFactory(props);
+        KafkaMessageListenerContainer<Integer, String> container = new KafkaMessageListenerContainer<>(cf, containerProperties);
+        return container;
+    }
+
+
+}
