f723b5c694e493ebd3ec5bb15eb8001644412b76
javis-yan <yan_h@hotmail.com>
2018-02-01 09:26:42 +0800

    remove @EnableBatchProcessing, provide own implementation with SimpleBatchConfiguration

--- src/main/java/com/nobodyhub/friday/crawler/person/PersonBatchConfiguration.java
+++ src/main/java/com/nobodyhub/friday/crawler/person/PersonBatchConfiguration.java
@@ -2,26 +2,40 @@ package com.nobodyhub.friday.crawler.person;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.SimpleBatchConfiguration;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.batch.core.configuration.support.MapJobRegistry;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.explore.support.MapJobExplorerFactoryBean;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobOperator;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
 
 /**
  * @author Ryan
  * @since 30/01/2018
  */
 @Configuration
-@EnableBatchProcessing
+//@EnableBatchProcessing
 @ComponentScan("com.nobodyhub.friday.crawler.person")
-public class PersonBatchConfiguration {
+public class PersonBatchConfiguration extends SimpleBatchConfiguration implements BatchConfigurer {
     @Autowired
     protected JobBuilderFactory jobBuilderFactory;
 
@@ -46,5 +60,66 @@ public class PersonBatchConfiguration {
                 .processor(processor)
                 .writer(writer)
                 .build();
+    }
+
+    @Bean
+    @Override
+    public PlatformTransactionManager getTransactionManager() throws Exception {
+        return new ResourcelessTransactionManager();
+    }
+
+    @Bean
+    public MapJobRepositoryFactoryBean mapJobRepositoryFactoryBean() throws Exception {
+        MapJobRepositoryFactoryBean mapJobRepositoryFactoryBean = new MapJobRepositoryFactoryBean();
+        mapJobRepositoryFactoryBean.setTransactionManager(getTransactionManager());
+        return mapJobRepositoryFactoryBean;
+    }
+
+
+    @Bean
+    @Override
+    public JobRepository getJobRepository() throws Exception {
+        return mapJobRepositoryFactoryBean().getObject();
+    }
+
+    @Bean
+    @Override
+    public JobLauncher getJobLauncher() throws Exception {
+        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+        jobLauncher.setJobRepository(getJobRepository());
+        jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        jobLauncher.afterPropertiesSet();
+        return jobLauncher;
+    }
+
+    @Bean
+    @Override
+    public JobExplorer getJobExplorer() throws Exception {
+        MapJobExplorerFactoryBean factoryBean = new MapJobExplorerFactoryBean();
+        factoryBean.setRepositoryFactory(mapJobRepositoryFactoryBean());
+        return factoryBean.getObject();
+    }
+
+    @Override
+    @Bean
+    public JobRegistry jobRegistry() throws Exception {
+        return new MapJobRegistry();
+    }
+
+    @Bean
+    public JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor() throws Exception {
+        JobRegistryBeanPostProcessor postProcessor = new JobRegistryBeanPostProcessor();
+        postProcessor.setJobRegistry(jobRegistry());
+        return postProcessor;
+    }
+
+    @Bean
+    public SimpleJobOperator jobOperator() throws Exception {
+        SimpleJobOperator jobOperator = new SimpleJobOperator();
+        jobOperator.setJobExplorer(jobExplorer());
+        jobOperator.setJobRegistry(jobRegistry());
+        jobOperator.setJobRepository(getJobRepository());
+        jobOperator.setJobLauncher(getJobLauncher());
+        return jobOperator;
     }
 }
