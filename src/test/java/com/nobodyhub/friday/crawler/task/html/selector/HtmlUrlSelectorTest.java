c8f309f23f256a61dd939066544e38e24224cf8f
javis-yan <yan_h@hotmail.com>
2018-03-11 11:05:16 +0800

    Add test for HtmlUrlSelector

---
+++ src/test/java/com/nobodyhub/friday/crawler/task/html/selector/HtmlUrlSelectorTest.java
@@ -0,0 +1,25 @@
+package com.nobodyhub.friday.crawler.task.html.selector;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Ryan
+ */
+public class HtmlUrlSelectorTest extends HtmlSelectorTest {
+    @Test
+    public void testSelect() {
+        this.selector = new HtmlUrlSelector("urlPattern", "a.mnav");
+        List<String> contents = this.selector.select(document);
+        assertEquals(6, contents.size());
+        assertEquals(true, contents.contains("http://news.baidu.com"));
+        assertEquals(true, contents.contains("http://www.hao123.com"));
+        assertEquals(true, contents.contains("http://map.baidu.com"));
+        assertEquals(true, contents.contains("http://v.baidu.com"));
+        assertEquals(true, contents.contains("http://tieba.baidu.com"));
+        assertEquals(true, contents.contains("http://xueshu.baidu.com"));
+    }
+}
