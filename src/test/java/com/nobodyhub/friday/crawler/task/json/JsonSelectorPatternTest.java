2a2600fc4637be0de178ed2d16a6dab99fd89aa5
javis-yan <yan_h@hotmail.com>
2018-03-20 22:32:53 +0800

    Tests for JsonSelectorPattern

--- src/test/java/com/nobodyhub/friday/crawler/task/json/JsonSelectorPatternTest.java
+++ src/test/java/com/nobodyhub/friday/crawler/task/json/JsonSelectorPatternTest.java
@@ -28,10 +28,20 @@ public class JsonSelectorPatternTest {
 
     @Test
     public void testSelect() {
-        JsonAttrSelectorPattern selector = new JsonAttrSelectorPattern("urlPattern", Lists.newArrayList("paging.next"));
+        JsonAttrSelectorPattern selector = new JsonAttrSelectorPattern("urlPattern", Lists.newArrayList("paging.next", "data[0].actor.url"));
         List<SelectorResult> contents = selector.select("http://www.zhihu.com", document);
-        assertEquals(1, contents.size());
-        //TODO
-//        assertEquals(true, contents.contains("https://www.zhihu.com/api/v4/members/zhang-jia-wei-64/activities?limit=8&after_id=1461208563&desktop=True"));
+        assertEquals(2, contents.size());
+        List<String> values = convertToAttrList(contents);
+        assertEquals(true, values.contains("https://www.zhihu.com/api/v4/members/zhang-jia-wei-64/activities?limit=8&after_id=1461208563&desktop=True"));
+        assertEquals(true, values.contains("https://api.zhihu.com/people/70e8e4eb769ecac38a805246ff1d268c"));
+
+    }
+
+    protected List<String> convertToAttrList(List<SelectorResult> results) {
+        List<String> values = Lists.newArrayList();
+        for (SelectorResult result : results) {
+            values.addAll(result.getAttributeMap().values());
+        }
+        return values;
     }
 }
